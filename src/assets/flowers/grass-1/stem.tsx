79fc10cdaf17ca784fdc2a260cbcddd55f574638
yuanjiling <69133976+yuanjiling@users.noreply.github.com>
2026-06-22 12:50:48 +0800

    delete

--- src/assets/flowers/grass-1/stem.tsx
+++
@@ -1,7 +0,0 @@
-export const FlowerStemBounds = { minX: 10.86, minY: 0, maxX: 209.07, maxY: 2000 } as const;
-
-export const FlowerStem = () => (
-  <>
-    <path d="M99.28,0c-.44,10.41,109.79,46.35-28.98,451.71-59.44,173.65-.06,420.11,15.19,887.68,7.61,233.32,16,583.51,12.86,660.61" fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth=".75px" />
-  </>
-);
